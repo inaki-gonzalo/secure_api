2a2e3324862fef347a94affb1af87615ee78a94a
Inaki Gonzalo <iaminakigonzalo@gmail.com>
2018-01-12 16:09:44 -0800

    restructured the api

--- src/api.php
+++ src/api.php
@@ -1,68 +1,57 @@
 <?php
-//This is an implementation of an authenticated extendable API
-
+// This is an implementation of an authenticated extendable API
 require_once 'Auth.php';
 require_once 'Connection.php';
 require_once 'Actions.php';
 
-
-#if HTTP Request is not POST return an error
+// if HTTP Request is not POST return an error
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    echo("Invalid Method");
-    exit;
+    echo ("Invalid Method");
+    exit();
 }
 
-
-function test_input($data) {
+function test_input($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
 
-
 // define variables and set to empty values
 $device_id = "";
 $device_key = "";
 $action = "";
 
-
-#Sanity check the inputs
+// Sanity check the inputs
 $device_id = test_input($_POST["device_id"]);
 $device_key = test_input($_POST["device_key"]);
 $action = test_input($_POST["action"]);
-if($device_id=="" || $device_key == "" || $action==""){
-	echo("Invalid Request");
-	exit;
-
+if ($device_id == "" || $device_key == "" || $action == "") {
+    echo ("Invalid Request");
+    exit();
 }
 
+// Validate credentials
+$auth = new Auth();
+$result = $auth->authenticate_device($device_id, $device_key);
 
-//Validate credentials
-$auth=new Auth();
-$result=$auth->authenticate_device($device_id, $device_key);
-
-if($result != TRUE){
-    exit;
+if ($result != TRUE) {
+    exit();
 }
 
+// Valid user and key
+// Once user is authenticated proccess the API request
 
-//Valid user and key
-//Once user is authenticated proccess the API request
-
-//Determine if the Action is valid
-$actions=new Actions();
+// Determine if the Action is valid
+$actions = new Actions();
 $APIActions = $actions->action_array;
-if (!in_array($action, $APIActions)) {
-	echo("Invalid Action");
-	exit;
+if (! in_array($action, $APIActions)) {
+    echo ("Invalid Action");
+    exit();
 }
 
-#Execute the appropriate action 
+// Execute the appropriate action
 $actions->$action();
 
-
-
-
 ?>
-
